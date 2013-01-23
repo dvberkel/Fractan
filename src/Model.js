9f7cc369c2551f713193eb31399b200d80909e37
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-01-23 21:24:49 +0100

    model should stop when no product is integral

--- src/Model.js
+++ src/Model.js
@@ -1,18 +1,28 @@
 (function(_, Backbone, Fractan, undefined){
     var FractanModel = Backbone.Model.extend({
+        defaults : { finished : false },
+
         N : function(){
             return this.get("N");
         },
 
+        finished : function(){
+            return this.get("finished");
+        },
+
         step : function(){
             var N = this.N();
-	    var result = _.first(
-		_.filter(
-		    _.map(this.get("fractions") || [], function(f){ return N.multiply(f); }),
-		    function(g){ return g.isIntegral() }
-		)
-	    );
-	    if (result) { this.set("N", result); }
+            var result = _.first(
+                _.filter(
+                    _.map(this.get("fractions") || [], function(f){ return N.multiply(f); }),
+                    function(g){ return g.isIntegral(); }
+                )
+            );
+            if (result) { 
+                this.set("N", result);
+            } else {
+                this.set("finished", true);
+            }
         }
     });
 
